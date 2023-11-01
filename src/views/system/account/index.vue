da23daa915ca70b032c460555938e38f50e9e58a
Luojh <mengqianyixing@163.com>
2023-11-01 16:14:38 +0800

    feat: 更正路径

--- src/views/system/account/index.vue
+++ src/views/system/account/index.vue
@@ -41,7 +41,7 @@
   import { defineComponent, reactive } from 'vue';
 
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
-  import { getAccountList } from '/@/api/demo/system';
+  import { getAccountList } from '/@/api/systemServer/system';
   import { PageWrapper } from '/@/components/Page';
   import DeptTree from './DeptTree.vue';
 
