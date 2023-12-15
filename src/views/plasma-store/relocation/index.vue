1a43692124322a6bab6bf87b9b317a38d2afc23d
zcc <971692082@qq.com>
2023-12-15 17:40:55 +0800

    feat: 托盘tabPane组件引入

--- src/views/plasma-store/relocation/index.vue
+++ src/views/plasma-store/relocation/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="h-full p-16px">
     <Tabs v-model:activeKey="activeKey" class="h-full bg-white">
-      <TabPane key="1" tab="血浆托盘出库">Content of Tab Pane 1</TabPane>
-      <TabPane key="2" tab="样品托盘出库" force-render>Content of Tab Pane 2</TabPane>
-      <TabPane key="3" tab="空托盘出库">Content of Tab Pane 3</TabPane>
-      <TabPane key="4" tab="托盘入库">Content of Tab Pane 3</TabPane>
-      <TabPane key="5" tab="托盘打印">Content of Tab Pane 3</TabPane>
-      <TabPane key="6" tab="托盘补打印">Content of Tab Pane 3</TabPane>
+      <TabPane key="1" tab="血浆托盘出库">
+        <PlasmaTray />
+      </TabPane>
+      <TabPane key="2" tab="样品托盘出库"><SampleTray /></TabPane>
+      <TabPane key="3" tab="空托盘出库"><EmpryTray /></TabPane>
+      <TabPane key="4" tab="托盘入库"><TrayInStore /></TabPane>
+      <TabPane key="5" tab="托盘打印"><TrayPrint /></TabPane>
     </Tabs>
   </div>
 </template>
 <script setup lang="ts">
   import { ref } from 'vue';
   import { TabPane, Tabs } from 'ant-design-vue';
+  import PlasmaTray from './plasmaTray.vue';
+  import SampleTray from './sampleTray.vue';
+  import EmpryTray from './emptyTray.vue';
+  import TrayInStore from './trayInStore.vue';
+  import TrayPrint from './trayPrint.vue';
 
   const activeKey = ref('1');
 </script>
