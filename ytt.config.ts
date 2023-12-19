b76b457fabf3c37fcf6dffccd9141478bdd5001d
Luojh <mengqianyixing@163.com>
2023-12-19 10:32:05 +0800

    types: 对接yapi标签管理类型

--- ytt.config.ts
+++ ytt.config.ts
@@ -62,6 +62,14 @@ export default defineConfig([
             },
             outputFilePath: 'src/api/type/resourcesManage.d.ts',
           },
+          {
+            id: 5208,
+            getRequestFunctionName(interfaceInfo, changeCase) {
+              // 以接口全路径生成请求函数名
+              return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
+            },
+            outputFilePath: 'src/api/type/tagManage.d.ts',
+          },
         ],
       },
       {
