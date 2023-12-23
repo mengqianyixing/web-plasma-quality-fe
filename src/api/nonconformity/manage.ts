507f471a20cc6e5c62d32201e8667d78c8b85c98
Luojh <mengqianyixing@163.com>
2023-12-23 21:01:06 +0800

    feat: 不合格登记接口对接

---
+++ src/api/nonconformity/manage.ts
@@ -0,0 +1,18 @@
+import { defHttp } from '@/utils/http/axios';
+import { PostApiCoreBagUnqualifiedRequest } from '@/api/type/nonconformityManage';
+
+enum Api {
+  NonconformityRegistration = '/api/core/bag/unqualified',
+}
+
+export const nonconformityRegistration = (params: PostApiCoreBagUnqualifiedRequest) => {
+  return defHttp.post(
+    {
+      url: Api.NonconformityRegistration,
+      params,
+    },
+    {
+      isReturnNativeResponse: true,
+    },
+  );
+};
