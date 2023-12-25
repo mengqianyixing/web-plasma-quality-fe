07c1a83be5333899980d2364cc2553e17c251082
Luojh <mengqianyixing@163.com>
2023-12-25 17:53:45 +0800

    feat: yapi文档对接

--- ytt.config.ts
+++ ytt.config.ts
@@ -78,6 +78,20 @@ export default defineConfig([
             },
             outputFilePath: 'src/api/type/dictionary.d.ts',
           },
+          {
+            id: 5236,
+            getRequestFunctionName(interfaceInfo, changeCase) {
+              return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
+            },
+            outputFilePath: 'src/api/type/encodingManage.d.ts',
+          },
+          {
+            id: 5215,
+            getRequestFunctionName(interfaceInfo, changeCase) {
+              return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
+            },
+            outputFilePath: 'src/api/type/printRecordManage.d.ts',
+          },
         ],
       },
       {
@@ -114,6 +128,20 @@ export default defineConfig([
               return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
             },
             outputFilePath: 'src/api/type/trayManage.d.ts',
+          },
+          {
+            id: 5299,
+            getRequestFunctionName(interfaceInfo, changeCase) {
+              return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
+            },
+            outputFilePath: 'src/api/type/nonconformityManage.d.ts',
+          },
+          {
+            id: 5313,
+            getRequestFunctionName(interfaceInfo, changeCase) {
+              return changeCase.camelCase(`${interfaceInfo.method}${interfaceInfo.path}`);
+            },
+            outputFilePath: 'src/api/type/stationManage.d.ts',
           },
         ],
       },
