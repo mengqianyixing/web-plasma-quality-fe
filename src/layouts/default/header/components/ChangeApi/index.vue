f8442509d5268a941ca7bc642c9049da74eeea85
Luojh <mengqianyixing@163.com>
2023-12-27 15:30:54 +0800

    feat: 支持输入API地址方便测试开发联调本地

--- src/layouts/default/header/components/ChangeApi/index.vue
+++ src/layouts/default/header/components/ChangeApi/index.vue
@@ -7,18 +7,17 @@
     @cancel="handelCancel"
   >
     <BasicForm @register="registerForm">
-      <template #api="{ model, field }">
-        <RadioGroup v-model:value="model[field]">
-          <Radio :style="radioStyle" :value="key" v-for="(val, key) in addresses" :key="key"
-            >{{ key }}: {{ val }}</Radio
-          >
-        </RadioGroup>
-      </template>
+      <!--      <template #api="{ model, field }">-->
+      <!--        <RadioGroup v-model:value="model[field]">-->
+      <!--          <Radio :style="radioStyle" :value="key" v-for="(val, key) in addresses" :key="key"-->
+      <!--            >{{ key }}: {{ val }}</Radio-->
+      <!--          >-->
+      <!--        </RadioGroup>-->
+      <!--      </template>-->
     </BasicForm>
   </BasicModal>
 </template>
 <script lang="ts" setup>
-  import { Radio } from 'ant-design-vue';
   import { useI18n } from '@/hooks/web/useI18n';
   import { BasicModal, useModalInner } from '@/components/Modal';
   import { BasicForm, useForm } from '@/components/Form';
@@ -27,22 +26,18 @@
   import type { ApiAddress } from '#/store';
 
   const appStore = useAppStore();
-  const RadioGroup = Radio.Group;
+  // const RadioGroup = Radio.Group;
   const { t } = useI18n();
   const [register, { closeModal }] = useModalInner(async () => {
     initData();
   });
   // perf 能读取所有.env.xxx文件最好, 另外key与--mode XXX最好相同
-  const addresses = ref({
-    development: 'http://www.a.com',
-    test: 'http://www.b.com',
-    prod: 'http://www.c.com',
-  });
-  const radioStyle = ref({
-    display: 'flex',
-    height: '30px',
-    lineHeight: '30px',
-  });
+  const addresses = ref('http://192.168.1.133:7100');
+  // const radioStyle = ref({
+  //   display: 'flex',
+  //   height: '30px',
+  //   lineHeight: '30px',
+  // });
   const [registerForm, { validateFields, setFieldsValue }] = useForm({
     showActionButtonGroup: false,
     schemas: [
@@ -52,10 +47,9 @@
         colProps: {
           span: 24,
         },
-        defaultValue: import.meta.env.MODE || 'development', // 当前环境
+        defaultValue: addresses.value, // 当前环境
         required: true,
         component: 'Input',
-        slot: 'api',
       },
     ],
   });
@@ -63,7 +57,7 @@
     const values = await validateFields();
     appStore.setApiAddress({
       key: values.api,
-      val: addresses.value[values.api],
+      val: values.api,
     });
     location.reload();
   };
