ce2e39a0737ff804f5bc670148d038bcfbe7fb40
Luojh <mengqianyixing@163.com>
2023-12-29 16:41:24 +0800

    feat: 血浆类型新增[炭疽]

--- src/enums/inspectEnum.ts
+++ src/enums/inspectEnum.ts
@@ -2,6 +2,7 @@ export enum PLASMA_TYPE {
   N = 'N',
   B = 'B',
   R = 'R',
+  A = 'A',
   T = 'T',
   C = 'C',
 }
@@ -9,6 +10,7 @@ export enum PLASMA_TYPE_TEXT {
   N = '普通',
   B = '乙免',
   R = '狂免',
+  A = '炭疽',
   T = '破免',
   C = '巨细胞',
 }
@@ -16,6 +18,7 @@ export const PLASMA_TYPE_LIST = [
   { value: PLASMA_TYPE.N, label: PLASMA_TYPE_TEXT.N },
   { value: PLASMA_TYPE.B, label: PLASMA_TYPE_TEXT.B },
   { value: PLASMA_TYPE.R, label: PLASMA_TYPE_TEXT.R },
+  { value: PLASMA_TYPE.A, label: PLASMA_TYPE_TEXT.A },
   { value: PLASMA_TYPE.T, label: PLASMA_TYPE_TEXT.T },
   { value: PLASMA_TYPE.C, label: PLASMA_TYPE_TEXT.C },
 ];
