c2cb2e43906b637d0218a641323b02855605f127
zcc <971692082@qq.com>
2024-01-03 21:48:00 +0800

    fix: defhttp uplaodFile 未拦截报错

--- src/utils/http/axios/Axios.ts
+++ src/utils/http/axios/Axios.ts
@@ -150,7 +150,7 @@ export class VAxios {
       });
     }
 
-    return this.axiosInstance.request<T>({
+    return this.request<T>({
       ...config,
       method: 'POST',
       data: formData,
