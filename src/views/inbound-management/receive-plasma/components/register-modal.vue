499b54193a0a6995e0877b80c63448c69392c47c
Chi YiFan <chiyf@stpass.com>
2024-01-06 15:05:46 +0800

    fix: 修复血浆接收、血浆验收bug

--- src/views/inbound-management/receive-plasma/components/register-modal.vue
+++ src/views/inbound-management/receive-plasma/components/register-modal.vue
@@ -23,13 +23,22 @@
         </FormItem>
       </Form>
     </div>
+    <template #footer>
+      <Button @click="hideModal">取消</Button>
+      <Button type="primary" :loading="loading" @click="confirm">登录</Button>
+    </template>
   </Modal>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { Modal, Form, FormItem, Input, InputPassword } from 'ant-design-vue';
+  import { Modal, Form, FormItem, Input, InputPassword, Button } from 'ant-design-vue';
   import { reCheckLogin } from '@/api/sys/login';
+  import { useUserStore } from '@/store/modules/user';
+  import { useMessage } from '@/hooks/web/useMessage';
+
+  const { createMessage } = useMessage();
+  const { warning } = createMessage;
 
   const emit = defineEmits(['close', 'login-data']);
 
@@ -45,18 +54,30 @@
   });
 
   const formRef = ref();
+  const loading = ref(false);
+  const userInfo = useUserStore();
 
   const confirm = () => {
     formRef.value
       .validate()
-      .then(() => {
-        reCheckLogin({
-          account: searchForm.value.username,
-          password: searchForm.value.password,
-        }).then((res: any) => {
-          emit('login-data', res);
-          hideModal();
-        });
+      .then(async () => {
+        try {
+          loading.value = true;
+          await reCheckLogin({
+            account: searchForm.value.username,
+            password: searchForm.value.password,
+          }).then((res: any) => {
+            if (res.username === userInfo.getUserInfo.username) {
+              warning('复核人不能与当前登录人相同');
+              return;
+            } else {
+              emit('login-data', res);
+              hideModal();
+            }
+          });
+        } finally {
+          loading.value = false;
+        }
       })
       .catch((error) => {
         console.log('error', error);
