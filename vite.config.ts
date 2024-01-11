671dd3ed5e00e6ab857d62ebc882a9127279d828
Luojh <mengqianyixing@163.com>
2024-01-11 16:39:50 +0800

    feat: 后端本地以及支持nginx 不需要不同服务不同转发路径（本文件proxy相关应该本地维护不提交）

--- vite.config.ts
+++ vite.config.ts
@@ -25,36 +25,11 @@ export default defineApplicationConfig({
     },
     server: {
       proxy: {
-        '/basic-api/api/core/bank/tr': {
-          // target:"http://192.168.1.81:8888",
-          // target: 'http://192.168.1.133:7001',
-          target: 'https://yapi.sharing8.cn/mock/529',
-          changeOrigin: true,
-          rewrite: (p) => p.replace(new RegExp(`^/basic-api`), ''),
-        },
-        '/basic-api/api/sys/dict': {
-          target: 'http://192.168.120.192:7002',
-          changeOrigin: true,
-          rewrite: (p) => p.replace(new RegExp(`^/basic-api`), ''),
-        },
-        '/basic-api/sys': {
-          target: 'http://192.168.120.192:7002/api',
-          changeOrigin: true,
-          ws: true,
-          rewrite: (path) => path.replace(new RegExp(`^/basic-api`), ''),
-        },
-        '/api/sys/': {
-          target: 'http://192.168.120.192:7002',
-          changeOrigin: true,
-          ws: true,
-        },
         '/basic-api': {
-          target: 'http://192.168.120.192:7002',
+          target: 'http://192.168.1.133:7100',
           changeOrigin: true,
           ws: true,
           rewrite: (path) => path.replace(new RegExp(`^/basic-api`), ''),
-          // only https
-          // secure: false
         },
         '/upload': {
           target: 'http://localhost:3300/upload',
