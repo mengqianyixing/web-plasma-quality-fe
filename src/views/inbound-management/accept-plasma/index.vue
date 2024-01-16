7092320af1593a37b9b7756c604d3d6271788746
Chi YiFan <chiyf@stpass.com>
2024-01-16 17:01:29 +0800

    fix: 血浆验收：详情栏样式优化

--- src/views/inbound-management/accept-plasma/index.vue
+++ src/views/inbound-management/accept-plasma/index.vue
@@ -156,7 +156,7 @@
       contentMinWidth: 100,
       render() {
         return (
-          <div class="flex items-center justify-center gap-2 w-[300px] -mt-1">
+          <div class="flex items-center justify-center gap-2 w-[250px] -mt-1">
             <a-input placeholder="请登录" value={checker} disabled />
             <a-button type="primary" onClick={handleLogin}>
               选择
@@ -171,7 +171,7 @@
       contentMinWidth: 100,
       render() {
         return (
-          <div class="flex items-center justify-center gap-2 w-[300px] -mt-1">
+          <div class="flex items-center justify-center gap-2 w-[250px] -mt-1">
             <a-input placeholder="请选择" value={batchNo} disabled />
             <a-button type="primary" onClick={selectBatchModal}>
               选择
@@ -186,7 +186,7 @@
       contentMinWidth: 100,
       render() {
         return (
-          <div class="flex items-center justify-center gap-2 w-[300px] -mt-1">
+          <div class="flex items-center justify-center gap-2 w-[250px] -mt-1">
             <a-input
               placeholder="请输入"
               onChange={(event) => (trayNo.value = event.target.value)}
@@ -202,7 +202,7 @@
       contentMinWidth: 100,
       render() {
         return (
-          <div class="flex items-center justify-center gap-2 w-[300px] -mt-1">
+          <div class="flex items-center justify-center gap-2 w-[250px] -mt-1">
             <a-input
               placeholder="请扫描"
               ref={bagNoRef}
