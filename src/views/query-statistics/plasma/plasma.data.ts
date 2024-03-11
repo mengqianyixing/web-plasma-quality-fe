2ab8ec29bf51a70c6d92656b84ca39a93ed22ea0
Luojh <mengqianyixing@163.com>
2024-03-11 10:10:25 +0800

    fix: 血浆查询-不合格原因显示问题

--- src/views/query-statistics/plasma/plasma.data.ts
+++ src/views/query-statistics/plasma/plasma.data.ts
@@ -100,7 +100,6 @@ export const columns: BasicColumn[] = [
   {
     title: '血浆不合格原因',
     dataIndex: 'plasmaUnqualifiedReason',
-    slots: { customRender: 'plasmaUnqualifiedReason' },
     width: 150,
   },
   {
