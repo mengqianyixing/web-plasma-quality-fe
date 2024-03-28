bca9690d9391aa06fd7367cd182d3661d84e7d8e
Luojh <mengqianyixing@163.com>
2024-03-28 16:42:35 +0800

    fix: 原料血浆入库缺失列表补充

--- src/views/query-statistics/plasma-raw-in-storage/index.vue
+++ src/views/query-statistics/plasma-raw-in-storage/index.vue
@@ -1,19 +1,58 @@
 <template>
-  <PageWrapper dense contentFullHeight fixedHeight class="root">
-    <BasicTable @register="registerTable" />
-  </PageWrapper>
+  <div class="h-full">
+    <div class="h-2/3 mb-50px">
+      <BasicTable @register="registerTable">
+        <template #toolbar>
+          <a-button type="primary">打印</a-button>
+        </template>
+      </BasicTable>
+    </div>
+    <div class="h-1/4 p-4">
+      <BasicTable
+        @register="registerFooterTable"
+        :dataSource="footerTableData"
+        :columns="formatFooterColumns"
+      >
+        <template #summary>
+          <div class="w-85vw">
+            备注；
+            {{ footerTableData[0]?.remark }}
+          </div>
+        </template>
+      </BasicTable>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-  import { BasicTable, useTable } from '@/components/Table';
-  import { columns, searchFormSchema } from './storage.data';
+  import { BasicColumn, BasicTable, useTable } from '@/components/Table';
+  import { columns, searchFormSchema, footerColumns } from './storage.data';
+  import { ref } from 'vue';
 
   import { getPlasmaRawInStorage } from '@/api/query-statistics/plasma';
-  import { PageWrapper } from '@/components/Page';
+  import { GetApiCoreBagOutInStorageStatisticResponse } from '@/api/type/queryStatistics';
 
   defineOptions({ name: 'PlasmaRawInStorage' });
 
-  const [registerTable] = useTable({
+  const footerTableData = ref<any[]>([]);
+  const formatFooterColumns = ref<BasicColumn[]>([]);
+  const [registerTable, { getRawDataSource }] = useTable({
     api: getPlasmaRawInStorage,
+    afterFetch: (data) => {
+      const _data: GetApiCoreBagOutInStorageStatisticResponse = getRawDataSource();
+
+      footerTableData.value = [
+        {
+          ..._data,
+        },
+      ];
+
+      formatFooterColumns.value = footerColumns.map((it) => ({
+        ...it,
+        title: it.title! + `(${_data.quantityUnit!})`,
+      }));
+
+      return data;
+    },
     columns,
     formConfig: {
       schemas: searchFormSchema,
@@ -30,17 +69,26 @@
     },
     clickToRowSelect: false,
     size: 'small',
-    canResize: true,
     striped: false,
     useSearchForm: true,
+    pagination: false,
     bordered: true,
     showIndexColumn: true,
-    indexColumnProps: {
-      className: 'content-none',
-    },
-    pagination: false,
     showSummary: true,
     summaryFunc: handleSummary,
+    isCanResizeParent: true,
+  });
+
+  const [registerFooterTable] = useTable({
+    clickToRowSelect: false,
+    size: 'small',
+    striped: false,
+    useSearchForm: false,
+    bordered: true,
+    pagination: false,
+    showSummary: true,
+    isCanResizeParent: true,
+    showIndexColumn: false,
   });
 
   function handleSummary(tableData: any[]) {
@@ -52,19 +100,7 @@
         inOfMonth,
         inOfYear,
       },
-      {
-        stationName: '总计',
-        inOfMonth,
-        inOfYear,
-      },
     ];
   }
 </script>
-<style scoped>
-  .root :deep(.ant-table-footer) {
-    position: sticky;
-    bottom: 3%;
-    padding: 0 !important;
-    border: 2px solid #f0f0f0 !important;
-  }
-</style>
+<style scoped></style>
