186447aa3566f80834a2305a01c57eae30dd3f40
Luojh <mengqianyixing@163.com>
2024-03-29 17:59:47 +0800

    fix: 【ID1108570【不合格血浆按站查询】未做导出功能】

--- src/views/query-statistics/unqualified-plasma-station/index.vue
+++ src/views/query-statistics/unqualified-plasma-station/index.vue
@@ -1,6 +1,10 @@
 <template>
   <PageWrapper dense contentFullHeight fixedHeight>
-    <BasicTable @register="registerTable" />
+    <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button type="primary" @click="handleExport" :loading="loading"> 导出 </a-button>
+      </template>
+    </BasicTable>
   </PageWrapper>
 </template>
 <script lang="ts" setup>
@@ -8,10 +12,18 @@
   import { columns, searchFormSchema } from './station.data';
   import { PageWrapper } from '@/components/Page';
   import { getUnqualifiedPlasmaStation } from '@/api/query-statistics/batch-statistics';
+  import { ref } from 'vue';
+  import { formatData, getHeader, jsonToSheetXlsx } from '@/components/Excel/src/Export2Excel';
+
+  import { useGlobalApiStoreWithOut } from '@/store/modules/globalApi';
+  import { useRouter } from 'vue-router';
+
+  const globalApiStore = useGlobalApiStoreWithOut();
+  const { currentRoute } = useRouter();
 
   defineOptions({ name: 'UnqualifiedPlasmaByStation' });
 
-  const [registerTable] = useTable({
+  const [registerTable, { getForm }] = useTable({
     api: getUnqualifiedPlasmaStation,
     columns,
     formConfig: {
@@ -28,4 +40,29 @@
     useSearchForm: true,
     bordered: true,
   });
+
+  const loading = ref(false);
+  async function handleExport() {
+    loading.value = true;
+    const pageSize = (await globalApiStore.getSysParamsValue('maxPageSize')) as string;
+
+    const OriginData = await getUnqualifiedPlasmaStation({
+      ...getForm().getFieldsValue(),
+      currPage: '1',
+      pageSize: pageSize,
+    });
+    loading.value = false;
+    const { rows, merges: headerMerge, lastLevelCols } = getHeader(columns);
+    const { result, merge: bodyMerge } = formatData(
+      lastLevelCols,
+      OriginData.result || [],
+      rows.length,
+    );
+    jsonToSheetXlsx({
+      data: [...rows, ...result],
+      json2sheetOpts: { skipHeader: true },
+      merges: [...headerMerge, ...bodyMerge],
+      filename: currentRoute.value.meta.title + '.xlsx',
+    });
+  }
 </script>
