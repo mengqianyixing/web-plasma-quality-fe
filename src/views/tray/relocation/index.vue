da9e0b1570933460f52c1835b0cf2b3c3f5434c9
zcc <971692082@qq.com>
2024-04-01 15:28:12 +0800

    feat: 隐藏样品箱解绑、绑定& 血浆检测数据结构变更

--- src/views/tray/relocation/index.vue
+++ src/views/tray/relocation/index.vue
@@ -24,7 +24,7 @@
       <TabPane key="5" tab="血浆箱手工解绑">
         <PlasmaBoxHand :is-binding="false" />
       </TabPane>
-      <TabPane key="6" tab="样本箱扫描绑定"> <SampleBoxScan :is-binding="true" /></TabPane>
+      <!-- <TabPane key="6" tab="样本箱扫描绑定"> <SampleBoxScan :is-binding="true" /></TabPane>
       <TabPane key="7" tab="样本箱扫描解绑">
         <SampleBoxScan :is-binding="false" />
       </TabPane>
@@ -33,7 +33,7 @@
       </TabPane>
       <TabPane key="9" tab="样本箱手工解绑">
         <SampleBoxHand :is-binding="false" />
-      </TabPane>
+      </TabPane> -->
     </Tabs>
   </PageWrapper>
 </template>
@@ -44,8 +44,8 @@
   import TrayRelocation from './trayRelocation.vue';
   import PlasmaBoxScan from './plasmaBoxScan.vue';
   import PlasmaBoxHand from './plasmaBoxHand.vue';
-  import SampleBoxScan from './sampleBoxScan.vue';
-  import SampleBoxHand from './sampleBoxHand.vue';
+  // import SampleBoxScan from './sampleBoxScan.vue';
+  // import SampleBoxHand from './sampleBoxHand.vue';
 
   defineOptions({ name: 'TrayRelocation' });
 
