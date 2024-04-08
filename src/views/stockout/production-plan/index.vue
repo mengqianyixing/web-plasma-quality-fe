0cfb22916df5fd6fd41039bbb885f356d67b778e
Luojh <mengqianyixing@163.com>
2024-04-08 16:12:02 +0800

    feat: 原料血浆摘要导出文件名优化

--- src/views/stockout/production-plan/index.vue
+++ src/views/stockout/production-plan/index.vue
@@ -346,7 +346,7 @@
     });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
-    a.download = '原料血浆摘要.docx';
+    a.download = `原料血浆摘要${row.mesId}.docx`;
     a.href = url;
     a.click();
     window.URL.revokeObjectURL(url);
