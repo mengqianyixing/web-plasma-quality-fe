c52daafd1ae389a1b781c372a487c625df604c78
YangCJ <vol@qq.com>
2024-04-18 17:25:39 +0800

    fix: 浆员查询column_value未完整显示的问题修复

--- src/views/query-statistics/donor/donor.data.ts
+++ src/views/query-statistics/donor/donor.data.ts
@@ -95,22 +95,22 @@ export const batchColumns: BasicColumn[] = [
   {
     title: '血浆批号',
     dataIndex: 'fkBatchNo',
-    width: 150,
+    width: 100,
   },
   {
     title: '浆站箱号',
     dataIndex: 'stationBoxNo',
-    width: 150,
+    width: 100,
   },
   {
     title: '现存箱号',
     dataIndex: 'boxNo',
-    width: 120,
+    width: 100,
   },
   {
     title: '血浆编号',
     dataIndex: 'bagNo',
-    width: 150,
+    width: 120,
   },
   {
     title: '采集日期',
@@ -123,22 +123,22 @@ export const batchColumns: BasicColumn[] = [
   {
     title: '来浆类型',
     dataIndex: 'rawImm',
-    width: 120,
+    width: 100,
   },
   {
     title: '浆站净重 g',
     dataIndex: 'rawWeight',
-    width: 100,
+    width: 90,
   },
   {
     title: '验收净重 g',
     dataIndex: 'netWeight',
-    width: 100,
+    width: 90,
   },
   {
     title: '效价类型',
     dataIndex: 'titerType',
-    width: 120,
+    width: 110,
     format: (text) =>
       (text || '')
         .split(',')
@@ -148,12 +148,12 @@ export const batchColumns: BasicColumn[] = [
   {
     title: '血浆过程状态',
     dataIndex: 'processState',
-    width: 120,
+    width: 210,
   },
   {
     title: '血浆不合格原因',
     dataIndex: 'failed',
-    width: 120,
+    width: 200,
   },
   {
     title: '血浆复检信息',
@@ -212,7 +212,7 @@ export const batchColumns: BasicColumn[] = [
         format(text) {
           return text ? dayjs(text).format('YYYY-MM-DD') : '-';
         },
-        width: 100,
+        width: 120,
       },
       {
         title: '样本批号',
@@ -222,7 +222,7 @@ export const batchColumns: BasicColumn[] = [
       {
         title: '样本编号',
         dataIndex: ['trackedSeeInfo', 'sampleNo'],
-        width: 135,
+        width: 130,
       },
       {
         title: '采集日期',
@@ -230,7 +230,7 @@ export const batchColumns: BasicColumn[] = [
         format(text) {
           return text ? dayjs(text).format('YYYY-MM-DD') : '-';
         },
-        width: 100,
+        width: 120,
       },
       {
         title: '浆站检验日期',
@@ -251,7 +251,7 @@ export const batchColumns: BasicColumn[] = [
       {
         title: '样本结果',
         dataIndex: ['trackedSeeInfo', 'reCheckResult'],
-        width: 100,
+        width: 90,
         format(text: any) {
           if (text === 1) {
             return '合格';
@@ -265,7 +265,7 @@ export const batchColumns: BasicColumn[] = [
       {
         title: '不合格项目',
         dataIndex: ['trackedSeeInfo', 'unqualifiedItems'],
-        width: 120,
+        width: 100,
       },
     ],
   },
