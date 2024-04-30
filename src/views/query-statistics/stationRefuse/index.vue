8214876964f1cf05ea8efb6c0d0862f052f06e95
YangCJ <vol@qq.com>
2024-04-30 10:08:24 +0800

    fix: 导出excel数据-浆站永柜查询

--- src/views/query-statistics/stationRefuse/index.vue
+++ src/views/query-statistics/stationRefuse/index.vue
@@ -16,19 +16,28 @@
           </a-button>
         </div>
       </template>
+      <template #toolbar>
+        <a-button type="primary" @click="handleExport" :loading="loading"> 导出 </a-button>
+      </template>
     </BasicTable>
   </PageWrapper>
 </template>
 <script lang="ts" setup>
-  import { onMounted, watchEffect } from 'vue';
+  import { onMounted, watchEffect, ref } from 'vue';
   import { useRouter } from 'vue-router';
   import { BasicTable, useTable } from '@/components/Table';
   import { columns, searchFormSchema } from './data';
   import { PageWrapper } from '@/components/Page';
   import { useStation } from '@/hooks/common/useStation';
   import { getStationRefuseList } from '@/api/query-statistics/stationRefuse.js';
+  import { formatData, getHeader, jsonToSheetXlsx } from '@/components/Excel/src/Export2Excel';
+  import { useGlobalApiStoreWithOut } from '@/store/modules/globalApi';
+  import { message } from 'ant-design-vue';
 
+  const { currentRoute } = useRouter();
+  const globalApiStore = useGlobalApiStoreWithOut();
   const router = useRouter();
+
   defineOptions({ name: 'StationRefuse' });
 
   const [registerTable, { getForm }] = useTable({
@@ -67,5 +76,36 @@
 
   function clickDonorNo(record) {
     router.push({ path: '/search/donor', query: { donorNo: record.donorNo } });
+  }
+
+  const loading = ref(false);
+  async function handleExport() {
+    loading.value = true;
+    try {
+      const { getFieldsValue } = getForm();
+      const pageSize = (await globalApiStore.getSysParamsValue('maxPageSize')) as string;
+      const data = await getStationRefuseList({
+        ...getFieldsValue(),
+        currPage: 1,
+        pageSize,
+      } as any);
+      if ((data.totalCount || 0) > Number(pageSize))
+        return message.warning('最多只能导出【' + pageSize + '】条数据');
+
+      const { rows, merges: headerMerge, lastLevelCols } = getHeader(columns);
+      const { result, merge: bodyMerge } = formatData(
+        lastLevelCols,
+        data.result || [],
+        rows.length,
+      );
+      jsonToSheetXlsx({
+        data: [...rows, ...result],
+        json2sheetOpts: { skipHeader: true },
+        merges: [...headerMerge, ...bodyMerge],
+        filename: currentRoute.value.meta.title + '.xlsx',
+      });
+    } finally {
+      loading.value = false;
+    }
   }
 </script>
