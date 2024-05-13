f738364be7aa42fa7871d7f038c42d9ccbd8993d
zcc <971692082@qq.com>
2024-05-13 09:44:43 +0800

    feat: 特免血浆统计去掉采集日期默认值

--- src/views/query-statistics/titerPlasmaStat/data.ts
+++ src/views/query-statistics/titerPlasmaStat/data.ts
@@ -4,7 +4,6 @@ import { SERVER_ENUM } from '@/enums/serverEnum';
 import { useServerEnumStoreWithOut } from '@/store/modules/serverEnums';
 import { formatKg } from '@/utils';
 import { useStation } from '@/hooks/common/useStation';
-import dayjs from 'dayjs';
 
 const { stationOptions } = useStation();
 const serverEnumStore = useServerEnumStoreWithOut();
@@ -168,7 +167,6 @@ export const searchFormSchema: FormSchema[] = [
     field: '[collectionBegin,collectionEnd]',
     component: 'RangePicker',
     label: '采集日期',
-    defaultValue: [dayjs().subtract(1, 'year'), dayjs()],
     componentProps: {
       class: 'w-full',
       valueFormat: 'YYYY-MM-DD',
