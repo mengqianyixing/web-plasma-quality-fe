bcd36c16c782831a85f995c86792308646a1bf0b
Luojh <mengqianyixing@163.com>
2024-05-15 11:26:12 +0800

    fix: 库存查询-日期选项必填

--- src/views/plasma-store/inventory/index.vue
+++ src/views/plasma-store/inventory/index.vue
@@ -134,6 +134,10 @@
       const values = getFieldsValue();
       tableLoading.value = true;
 
+      if (!values.date) {
+        return createMessage.warning('请选择日期');
+      }
+
       if (values.dateKey === 'receipt' && values.date) {
         values.receiptStartDate = values.date[0];
         values.receiptEndDate = values.date[1];
@@ -202,7 +206,7 @@
     delete header['undefined'];
     jsonToSheetXlsx<any>({
       header,
-      filename: `库存查询-${dateFlag}.xlsx`,
+      filename: `库存${values.date[0]}-${values.date[1]}${dateFlag}.xlsx`,
       data: originExportData.map((it) => {
         return omit(it, ['inWeightG', 'outWeightG']);
       }),
