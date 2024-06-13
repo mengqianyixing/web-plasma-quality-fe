8bf0e65545056e2567acd0379a0afa542f7a8157
zcc <971692082@qq.com>
2024-06-13 10:53:48 +0800

    feat: 检测结果改为search服务

--- src/api/query-statistics/checkResult.ts
+++ src/api/query-statistics/checkResult.ts
@@ -8,13 +8,13 @@ import {
 } from '@/api/type/queryStatistics';
 
 enum Api {
-  LIST = '/api/core/lab/samples',
+  LIST = '/api/search/sample/result',
   DT = '/api/core/lab/samples/lab/item',
   UN_DT = '/api/core/lab/samples/lab/unqualified/item',
 }
 
 export const getListApi = (params: PostApiCoreLabSamplesRequest) =>
-  defHttp.post<PostApiCoreLabSamplesResponse>({ url: Api.LIST, params });
+  defHttp.get<PostApiCoreLabSamplesResponse>({ url: Api.LIST, params });
 
 export const getDtApi = (params: PostApiCoreLabSamplesLabItemRequest) =>
   defHttp.post<PostApiCoreLabSamplesLabItemResponse>({ url: Api.DT, params });
