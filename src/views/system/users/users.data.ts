ddc348d68ba820b033e9fcef4780d03c749b1746
詹成才 <971692082@qq.com>
2024-07-09 15:18:32 +0800

    feat: 修改密码需要旧密码

--- src/views/system/users/users.data.ts
+++ src/views/system/users/users.data.ts
@@ -175,6 +175,12 @@ export const passwordFormSchema: FormSchema[] = [
     },
   },
   {
+    field: 'oldPassword',
+    label: '旧密码',
+    component: 'InputPassword',
+    required: true,
+  },
+  {
     field: 'newPassword',
     label: '密码',
     component: 'InputPassword',
