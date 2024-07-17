8868c416c6c1335c118a8136c8b78dc883a054bf
詹成才 <971692082@qq.com>
2024-07-17 17:42:17 +0800

    feat: doc3-113 采浆公司,样本批号排序

--- src/views/query-statistics/callback-batch/batch.data.ts
+++ src/views/query-statistics/callback-batch/batch.data.ts
@@ -7,36 +7,46 @@ export const columns: BasicColumn[] = [
   {
     title: '采浆公司',
     dataIndex: 'stationName',
+    sorter: true,
+    width: 100,
   },
   {
     title: '样本批号',
     dataIndex: 'sampleBatchNo',
+    sorter: true,
+    width: 120,
+    ellipsis: false,
   },
 
   {
     title: '回访样本数量',
     dataIndex: 'sampleNum',
+    width: 110,
   },
   {
     title: '检测结果审核人',
     dataIndex: 'reviewer',
+    width: 110,
   },
   {
     title: '接收日期',
     dataIndex: 'receiveDate',
+    width: 100,
   },
   {
     title: '采集日期起止',
     dataIndex: 'collectDateString',
-    width: 200,
+    width: 220,
   },
   {
     title: '检测结果发布人',
     dataIndex: 'issueBy',
+    width: 130,
   },
   {
     title: '检测结果发布日期',
     dataIndex: 'issueDate',
+    width: 130,
   },
 ];
 
