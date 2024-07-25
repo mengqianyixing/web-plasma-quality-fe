7f6a37051537b62161854fab432da116090dd1fc
Luojh <mengqianyixing@163.com>
2024-07-25 17:54:58 +0800

    fix: 回访名单-缓存问题

--- src/views/callback/list-generation/CallbackDetailModal.vue
+++ src/views/callback/list-generation/CallbackDetailModal.vue
@@ -6,6 +6,7 @@
     width="85%"
     :min-height="600"
     :showOkBtn="false"
+    @cancel="handleCancel"
   >
     <div class="relative h-inherit max-h-inherit min-h-inherit">
       <div class="absolute flex flex-col w-full h-full">
@@ -103,7 +104,7 @@
           )
       : callbackDetailModalColumns;
   });
-  const [registerTable, { reload }] = useTable({
+  const [registerTable, { reload, getForm }] = useTable({
     api: getCallbackDetail,
     formConfig: {
       schemas: callbackDetailFormSchema,
@@ -139,11 +140,7 @@
     unref(isPreview) ? '名单详情' : unref(isUpdate) ? '编辑名单' : '生成名单',
   );
 
-  const [register, { setModalProps }] = useModalInner((data) => {
-    setModalProps({
-      maskClosable: false,
-      destroyOnClose: true,
-    });
+  const [register, { closeModal }] = useModalInner((data) => {
     isUpdate.value = data.isUpdate;
     isPreview.value = data.isPreview;
     batchNo.value = data.planNo;
@@ -159,5 +156,10 @@
       cardNo: _record.cardNo,
       planNo: batchNo.value,
     });
+  }
+
+  function handleCancel() {
+    getForm().resetFields();
+    closeModal();
   }
 </script>
