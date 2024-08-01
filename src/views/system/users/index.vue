6a989560439a1f7a10ff9eee258b3ad9db94ea60
詹成才 <971692082@qq.com>
2024-08-01 10:01:57 +0800

    feat: doc4-39 导出用户角色

--- src/views/system/users/index.vue
+++ src/views/system/users/index.vue
@@ -2,6 +2,7 @@
   <div>
     <BasicTable @register="registerTable">
       <template #toolbar>
+        <a-button type="success" @click="handleExport">导出</a-button>
         <a-button type="primary" @click="handleCreate">新增用户</a-button>
       </template>
       <template #bodyCell="{ column, record }">
@@ -47,11 +48,14 @@
     getCasDoorUsers,
     resetCasDoorUserPwd,
   } from '@/api/oauth/users';
+  import { getCasDoorRoles, getCasDoorAllUsers } from '@/api/oauth/auth';
+
   import { ExclamationCircleOutlined } from '@ant-design/icons-vue';
   import { useMessage } from '@/hooks/web/useMessage';
   import { useModal } from '@/components/Modal';
   import UsersModal from './UsersModal.vue';
   import UsersDetailModal from './UsersDetailModal.vue';
+  import { exportFile, transferCSVData } from 'js-xxx';
 
   import { columns, searchFormSchema } from './users.data';
 
@@ -61,7 +65,7 @@
 
   const [registerModal, { openModal }] = useModal();
   const [registerUserDetailModal, { openModal: openUsersDetailModal }] = useModal();
-  const [registerTable, { reload }] = useTable({
+  const [registerTable, { reload, getSelectRows }] = useTable({
     api: getCasDoorUsers,
     fetchSetting: {
       pageField: 'currPage',
@@ -85,12 +89,33 @@
       // slots: { customRender: 'action' },
       fixed: undefined,
     },
+    rowSelection: { type: 'checkbox' },
   });
 
   function handleCreate() {
     openModal(true, {
       isUpdate: false,
     });
+  }
+
+  async function handleExport() {
+    const rows = getSelectRows();
+    if (rows.length === 0) return createMessage.warn('请选择一条记录');
+    const userList = await getCasDoorAllUsers({ userIds: rows.map((it) => it.name) });
+    const rolesRes = await getCasDoorRoles({ currPage: 1, pageSize: 999 });
+    const { result: roles } = rolesRes;
+    const exportData: any[] = [];
+    const excelCol = [{ label: '用户', prop: 'username' }];
+    roles.forEach((it) => excelCol.push({ label: it.displayName, prop: it.displayName }));
+    userList.forEach((it) => {
+      const obj = (it?.roles || []).reduce((pre, cur) => {
+        pre[cur.displayName] = '√';
+        return pre;
+      }, {});
+      exportData.push({ username: it.name, ...obj });
+    });
+    exportFile(transferCSVData(excelCol, exportData), `用户角色`, 'csv');
+    createMessage.success('导出成功');
   }
 
   async function handleEdit(record: Recordable) {
