6aae85d358d64cf6f3814a683100543bfc8ff7b0
zcc <971692082@qq.com>
2024-11-12 13:44:02 +0800

    feat: doc-uat35 按钮权限未绑定

--- src/views/quality-manage/batch-release/index.vue
+++ src/views/quality-manage/batch-release/index.vue
@@ -55,17 +55,19 @@
         >
           打印
         </a-button>
-        <span>
+        <span
+          v-auth="[QualityButtonEnum.BatchReleasePrintKM, QualityButtonEnum.BatchQuarantinePeriod]"
+        >
           <a-dropdown>
             <a-button type="primary" :loading="reportLoading"> 打印 </a-button>
             <template #overlay>
               <Menu>
-                <span>
+                <span v-auth="QualityButtonEnum.BatchReleasePrintKM">
                   <MenuItem @click="handlePrint">
                     <a-button type="link" :loading="reportLoading"> 原料血浆批审核放行单 </a-button>
                   </MenuItem>
                 </span>
-                <span>
+                <span v-auth="QualityButtonEnum.BatchQuarantinePeriod">
                   <MenuItem @click="handlePrintQuarantine">
                     <a-button type="link" :loading="reportLoading">
                       原料血浆检疫期筛选情况
